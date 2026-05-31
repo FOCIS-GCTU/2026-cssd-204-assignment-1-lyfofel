68e3deec237e9c456c621b11b29c6e2624cc7d14
lyfofel <quaynorelvis08@gmail.com>
2026-05-31 00:15:20 +0000

    Add function to display initials in block letters
    Implement function to print stylized initials QEN in a grid format.

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name:Quaynor Elvis Nii
+# STUDENT ID:2425402720
+# Email: 2425402720@gctu.edu.gh
+# Grader: Elvis
+#
+# On my honor, Quaynor Elvis Nii, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Prints out the stylized QEN initials according to the strict 60x10 grid rules
+    print()
+    print("...QEN")
+    print()
+    print("...  QQQQQQQQ  ...  ...EEEEEEEEEEEE...  ...NN        NN...")
+    print("...QQQQQQQQQQQQ...  ...EEEEEEEEEEEE...  ...NNN       NN...")
+    print("...QQ        QQ...  ...EE          ...  ...NNNN      NN...")
+    print("...QQ        QQ...  ...EE          ...  ...NN NN     NN...")
+    print("...QQ        QQ...  ...EEEEEEEEEEEE...  ...NN  NN    NN...")
+    print("...QQ     QQ QQ...  ...EEEEEEEEEEEE...  ...NN   NN   NN...")
+    print("...QQ      QQQQ...  ...EE          ...  ...NN    NN  NN...")
+    print("...QQQQQQQQQQQQ...  ...EE          ...  ...NN     NN NN...")
+    print("...  QQQQQQQQ  ...**...EEEEEEEEEEEE...**...NN      NNNN...**")
+    print("...        QQQQ...**...EEEEEEEEEEEE...**...NN        NN...**")
+    print()
 
 
 main()
